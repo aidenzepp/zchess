9523c4dd6712d7850edfc2a3e6936607ce8cf6b4
Aiden Zepp <aiden.zepp@protonmail.com>
2024-11-03 21:46:47 -0500

    bitboard

--- src/engine.c
+++ src/engine.c
@@ -3,8 +3,13 @@
 #include <stddef.h>
 
 #define COLOR_BIT_MASK  0x18
-#define PIECE_BIT_MASK  0x0F
+#define PIECE_BIT_MASK  0x07
 #define PIECE_BIT_WIDTH 3
+
+enum color invert_color(enum color color)
+{
+        return (color == COLOR_WHITE) ? COLOR_BLACK : COLOR_WHITE;
+}
 
 bool decode_color(uint8_t bits, enum color *color)
 {
@@ -34,4 +39,27 @@ uint8_t encode_color(uint8_t bits, enum color color)
 uint8_t encode_piece(uint8_t bits, enum piece piece)
 {
         return bits | (uint8_t)piece;
+}
+
+bitboard_t obtain_color_bitboard(struct board board, enum color color)
+{
+        return board.pieces[(int)color - 1];
+}
+
+bitboard_t obtain_piece_bitboard(struct board board, enum piece piece)
+{
+        return board.pieces[(int)piece + 1];
+}
+
+bitboard_t obtain_board(struct board board, enum color *color, enum piece *piece)
+{
+        bitboard_t color_bb = 0;
+        if (color != NULL)
+                color_bb = obtain_color_bitboard(board, *color);
+
+        bitboard_t piece_bb = 0;
+        if (piece != NULL)
+                piece_bb = obtain_piece_bitboard(board, *piece);
+
+        return color_bb & piece_bb;
 }
--- src/engine.h
+++ src/engine.h
@@ -9,7 +9,7 @@ enum color {
 };
 
 enum piece {
-        PIECE_KING,
+        PIECE_KING = 1,
         PIECE_QUEEN,
         PIECE_ROOK,
         PIECE_BISHOP,
@@ -17,7 +17,50 @@ enum piece {
         PIECE_PAWN,
 };
 
-bool    decode_color(uint8_t bits, enum color *color);
-bool    decode_piece(uint8_t bits, enum piece *piece);
-uint8_t encode_color(uint8_t bits, enum color color);
-uint8_t encode_piece(uint8_t bits, enum piece piece);
+enum rank {
+        RANK_1,
+        RANK_2,
+        RANK_3,
+        RANK_4,
+        RANK_5,
+        RANK_6,
+        RANK_7,
+        RANK_8,
+};
+
+enum file {
+        FILE_A,
+        FILE_B,
+        FILE_C,
+        FILE_D,
+        FILE_E,
+        FILE_F,
+        FILE_G,
+        FILE_H,
+};
+
+enum tier {
+        TIER_S,
+        TIER_T,
+        TIER_U,
+        TIER_V,
+        TIER_W,
+        TIER_X,
+        TIER_Y,
+        TIER_Z,
+};
+
+typedef uint64_t bitboard_t;
+
+struct board {
+        bitboard_t pieces[8];
+};
+
+enum color invert_color(enum color color);
+bool       decode_color(uint8_t bits, enum color *color);
+bool       decode_piece(uint8_t bits, enum piece *piece);
+uint8_t    encode_color(uint8_t bits, enum color color);
+uint8_t    encode_piece(uint8_t bits, enum piece piece);
+bitboard_t obtain_color_bitboard(struct board board, enum color color);
+bitboard_t obtain_piece_bitboard(struct board board, enum piece piece);
+bitboard_t obtain_board(struct board board, enum color *color, enum piece *piece);
