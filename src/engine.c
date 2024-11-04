3920d5b8edbea5455af4d8fdae40bc10b17d41a4
Aiden Zepp <aiden.zepp@protonmail.com>
2024-11-04 07:12:24 -0500

    added popcount and scanning

--- src/engine.c
+++ src/engine.c
@@ -5,6 +5,10 @@
 #define COLOR_BIT_MASK  0x18
 #define PIECE_BIT_MASK  0x07
 #define PIECE_BIT_WIDTH 3
+#define BITBOARD_NIL    ((bitboard_t)0)
+#define BITBOARD_ALL    ((bitboard_t)0xFFFFFFFFFFFFFFFF)
+
+#define BITBOARD
 
 enum color invert_color(enum color color)
 {
@@ -24,7 +28,7 @@ bool decode_color(uint8_t bits, enum color *color)
 bool decode_piece(uint8_t bits, enum piece *piece)
 {
         uint8_t piece_bits = (bits & PIECE_BIT_MASK);
-        if (piece_bits == 0)
+        if (piece_bits < 1 || piece_bits > 6)
                 return false;
 
         *piece = (enum piece)piece_bits;
@@ -41,6 +45,61 @@ uint8_t encode_piece(uint8_t bits, enum piece piece)
         return bits | (uint8_t)piece;
 }
 
+int obtain_piece_popcount(bitboard_t bitboard)
+{
+        // https://www.chessprogramming.org/Population_Count
+        const bitboard_t K1 = (bitboard_t)0x5555555555555555;
+        const bitboard_t K2 = (bitboard_t)0x3333333333333333;
+        const bitboard_t K4 = (bitboard_t)0x0f0f0f0f0f0f0f0f;
+        const bitboard_t KF = (bitboard_t)0x0101010101010101;
+
+        bitboard = (bitboard - ((bitboard >> 1) & K1);
+        bitboard = (bitboard & k2) + ((bitboard >> 2) & K2);
+        bitboard = (bitboard + (bitboard >> 4)) & K4;
+        bitboard = (bitboard * KF) >> 56;
+
+        return (int)bitboard;
+}
+
+int forward_scan_bitboard(bitboard_t bitboard)
+{
+        if (bitboard == 0)
+                return -1;
+
+        return obtain_piece_popcount((bitboard & -bitboard) - 1);
+}
+
+int reverse_scan_bitboard(bitboard_t bitboard)
+{
+        int ret = 0;
+        if (bitboard > 0xFFFFFFFF) {
+                bitboard >>= 32;
+                ret = 32;
+        }
+        if (bitboard > 0xFFFF) {
+                bitboard >>= 16;
+                ret += 16;
+        }
+        if (bitboard > 0xFF) {
+                bitboard >>= 8;
+                ret += 8;
+        }
+
+        // TODO: Implement ms1b_table (https://www.talkchess.com/forum/viewtopic.php?t=38777).
+        return ret + ms1b_table[bitboard];
+}
+
+int general_scan_bitboard(bitboard_t bitboard, bool reverse)
+{
+        if (bitboard == 0)
+                return -1;
+
+        bitboard_t rmask = -(bitboard_t)reverse;
+        bitboard &= -bitboard | rmask;
+
+        return reverse_scan_bitboard(bitboard);
+}
+
 bitboard_t obtain_color_bitboard(struct board board, enum color color)
 {
         return board.pieces[(int)color - 1];
@@ -49,17 +108,4 @@ bitboard_t obtain_color_bitboard(struct board board, enum color color)
 bitboard_t obtain_piece_bitboard(struct board board, enum piece piece)
 {
         return board.pieces[(int)piece + 1];
-}
-
-bitboard_t obtain_board(struct board board, enum color *color, enum piece *piece)
-{
-        bitboard_t color_bb = 0;
-        if (color != NULL)
-                color_bb = obtain_color_bitboard(board, *color);
-
-        bitboard_t piece_bb = 0;
-        if (piece != NULL)
-                piece_bb = obtain_piece_bitboard(board, *piece);
-
-        return color_bb & piece_bb;
 }
